748db4f94c4dab4ea3f7a8a2789c719f788b8e23
HongSh1991 <hjmrezl@outlook.com>
2018-02-09 21:27:48 +0800

    add gitignore file

--- blog/apps.py
+++ blog/apps.py
@@ -3,5 +3,5 @@ from __future__ import unicode_literals
 from django.apps import AppConfig
 
 
-class MyblogConfig(AppConfig):
-    name = 'myblog'
+class BlogConfig(AppConfig):
+    name = 'blog'
--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,19 @@
 from __future__ import unicode_literals
 
 from django.db import models
+import django.utils.timezone
+from django.contrib.auth.models import User
 
 
 # Create your models here.
+class BlogArticles(models.Model):
+    title = models.CharField(max_length=30)
+    author = models.ForeignKey(User, related_name="blog_posts")
+    body = models.TextField()
+    publish = models.DateTimeField(default=django.utils.timezone.now())
+
+    class Meta:
+        ordering = ("-publish",)
+
+    def __str__(self):
+        return self.title
